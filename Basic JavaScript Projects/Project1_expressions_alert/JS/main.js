d8872164aef848de4aec03cff96d92a48d1c70b8
jrheat2005 <jrheat2005@gmail.com>
2023-07-09 16:29:53 -0700

    New Project

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+
+window.alert("Hello Welcome to My Project");
+var Sent1 = "This is the beginning of the string ";
+var Sent2="and this is the end of the string ";
+document.write(Sent1 + Sent2);
+document.write(5 - 4);
+function This_Function() { //Button function
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
