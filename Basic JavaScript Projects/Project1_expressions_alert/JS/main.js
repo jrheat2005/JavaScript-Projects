c9ce5cadb58309210a9fb085fc257f26e5a0c696
jrheat2005 <jrheat2005@gmail.com>
2023-07-09 18:44:09 -0700

    added JavaScript Project 2

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -5,6 +5,7 @@ var Sent2="and this is the end of the string ";
 document.write(Sent1 + Sent2);
 document.write(5 - 4);
 function This_Function() { //Button function
-    var str = "This is the button text!";
-    document.getElementById("Button_Text").innerHTML = str;
+    var str = "This is the button text! ";
+    var stl = "This is Interesting";
+    document.getElementById("Button_Text").innerHTML = str + stl;
 }
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function This_Function() { //Button function
+    var str = "This is the button text! "; //First Text
+    var stl = "This is Interesting"; //Second Text
+    document.getElementById("Button_Text").innerHTML = str + stl; //id name and concantenating the sentences
+}
+function This_One() { //decalring function
+var slv = "Hello"; //first sentence
+slv += " this is crazy"; //second sentence
+document.getElementById("Second").innerHTML = slv; // id name and concantenating the sentences
+}
