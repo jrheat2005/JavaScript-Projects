6578bbc03dc843d48d9209f65ff5d77a528203b5
jrheat2005 <jrheat2005@gmail.com>
2023-07-12 16:04:42 -0700

    added comments

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,43 +1,44 @@
-function countdown() {
+function countdown() { 
     var seconds = document.getElementById("seconds").value;
     function tick() {
-        seconds = seconds -1;
-        timer.innerHTML = seconds;
-        var time = setTimeout(tick, 1000);
-        if (seconds == -1) {
+        seconds = seconds -1; //tick the seconds down by 1
+        timer.innerHTML = seconds; //timer equlas the second
+        var time = setTimeout(tick, 1000); //1 second tick down
+        if (seconds == -1) { //if less than 1 alert
             alert("Time's up!");
-            clearTimeout(time);
+            clearTimeout(time); //clear timer
             timer.innerHTML = "";
         }
     }
     tick();
 }
-
+// index number
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+// Next or back controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+// image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  let slides = document.getElementsByClassName("mySlides"); //all slide elemtents
+  let dots = document.getElementsByClassName("dot"); //all dot elements
+  if (n > slides.length) {slideIndex = 1} //if the index is higher than 3 return to 1
+  if (n < 1) {slideIndex = slides.length} //if less than 1 go back to 3
+  for (i = 0; i < slides.length; i++) { //hide slides
+    slides[i].style.display = "none"; 
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < dots.length; i++) {  //remove the active class
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  //display the slide and the active dot for each slide
+  slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
